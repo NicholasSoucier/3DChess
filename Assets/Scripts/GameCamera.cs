f8395c530ec40aea2277160f0e68abad18ab18e3
DavidFavela <david.favela.co@gmail.com>
2022-11-28 13:51:32 -0600

    Added Room

--- Assets/Scripts/GameCamera.cs
+++ Assets/Scripts/GameCamera.cs
@@ -6,6 +6,12 @@ public class GameCamera : MonoBehaviour
 {
     public Transform target;
     public float speed = 1;
+    public Transform whiteTurnCamPosition;
+    public Transform blackTurnCamPosition;
+    private Quaternion whiteOrientation = Quaternion.Euler(0, 270, 0);
+    private Quaternion blackOrientation = Quaternion.Euler(0, 90, 0);
+
+
     private void Update()
     {
         if (target) {
@@ -15,16 +21,24 @@ public class GameCamera : MonoBehaviour
         }
     }
 
-    public void SetPosition(Transform _point) {
+    public void SetPosition(Transform to_point) {
 
-        StartCoroutine(UpdateTargetAfterTime(_point));
+        StartCoroutine(UpdateTargetAfterTime(to_point));
     }
 
-    IEnumerator UpdateTargetAfterTime(Transform _point) {
+    /*public void SetTurn(bool isWhite)
+    {
+        if (isWhite)
+        {
+            SetPosition();
+        }
+    }*/
+
+    IEnumerator UpdateTargetAfterTime(Transform to_point) {
 
         yield return new WaitForSeconds(0.5f);
 
-        target = _point;
+        target = to_point;
 
     }
 }
